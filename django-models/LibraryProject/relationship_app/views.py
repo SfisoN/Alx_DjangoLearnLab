c27c5915fb8c2a0e7ace2e6a78b64b3f1c40a2d2
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-11-08 11:29:12 +0200

    Haii

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import DetailView
 
-from .models import Book, Library, Librarian
+from .models import Library
 
 # Create your views here.
 
