dc7de6ea2b3362112fdbe0de343b38aaa07e64e6
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-11-08 12:16:38 +0200

    new commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,11 +15,12 @@ urlpatterns = [
     path("register/", views.register, name="register"),  # ✅ views.register
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),  # ✅ LoginView.as_view(template_name=
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),  # ✅ LogoutView.as_view(template_name=
+
     path("admin-view/", admin_view.admin_view, name="admin_view"),
     path("librarian-view/", librarian_view.librarian_view, name="librarian_view"),
     path("member-view/", member_view.member_view, name="member_view"),
 
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/<int:pk>/edit/", views.edit_book, name="edit_book"),
-    path("books/<int:pk>/delete/", views.delete_book, name="delete_book"),
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
