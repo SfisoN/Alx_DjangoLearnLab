a9a161d4fd1e738549205568581a1998b5c7f13c
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-11-16 15:21:50 +0200

    IDK

--- advanced_features_and_security/accounts/admin.py
+++ advanced_features_and_security/accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from ..LibraryProject.bookshelf.models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
