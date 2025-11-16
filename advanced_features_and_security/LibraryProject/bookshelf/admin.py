91c27a072975eb48be632e6cad9aa204a6a19622
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-11-16 15:24:49 +0200

    idk

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
-from .models import Book
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from ...accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('publication_year', 'author')
-    search_fields = ('title', 'author')
-    ordering = ('publication_year', 'title')
-    list_editable = ('author', 'publication_year')
-    list_per_page = 25
+@admin.site.register(CustomUser,)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "date_of_birth")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+    # Add custom fields to the user detail view
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+    # Include the extra fields on the add user form
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+
