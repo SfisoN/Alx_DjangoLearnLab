2a7e3395ba103bd25515963d7a0d15f469d3d922
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-12-29 15:08:03 +0200

    new update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -85,8 +85,12 @@ STATICFILES_DIRS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',
+        'USER': 'django_blog_user',
+        'PASSWORD': 'django_blog_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
