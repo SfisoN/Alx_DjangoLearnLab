8f6757c17033823efb5a3794e9b2262cfccd7034
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-12-29 14:16:17 +0200

    idk new update

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
